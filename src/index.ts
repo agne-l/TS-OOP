0484af52e28b4e13f9fda453c6854d239ce0e7fb
Agne Laurinaviciute <agnelaurinaviciute26@gmail.com>
2023-09-22 15:00:48 +0300

    deleted unnecessary logs in index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,31 +7,28 @@ import { Zookeeper } from "./Zookeeper.js";
 const zebra = new Zebra("Stripes", 5, "40km/h", "Africa");
 const elephant = new Elephant("Peanut", 10, "5t");
 const tiger = new Tiger("Fluffy", 5, false);
-
-console.log(zebra);
-console.log(elephant);
-console.log(tiger);
-
-const zebraName = zebra.getName();
-console.log(zebraName);
-
-const elephantName = elephant.getName();
-console.log(elephantName);
-
-const tigerName = tiger.getName();
-console.log(tigerName);
+const tiger2 = new Tiger("Tiggy", 6, false);
 
 const zoo = Zoo.getInstance();
 zoo.addAnimal(zebra);
-console.log(zoo);
+zoo.addAnimal(elephant);
+zoo.addAnimal(tiger);
+zoo.addAnimal(tiger2);
 
-console.log(zoo.getAllAnimals());
+const allAnimals = zoo.getAllAnimals();
+console.log(allAnimals);
 
 const zookeeper = new Zookeeper();
 
+const zebraName = zebra.getName();
+const elephantName = elephant.getName();
+const tigerName = tiger.getName();
+const tiger2Name = tiger2.getName();
+
 zookeeper.feedAnimal(zebraName);
-// zookeeper.feedAnimal(elephantName);
-// zookeeper.feedAnimal(tigerName);
+zookeeper.feedAnimal(elephantName);
+zookeeper.feedAnimal(tigerName);
+zookeeper.feedAnimal(tiger2Name);
 
 const feedingLog = zookeeper.getFeedingLog();
 console.log(feedingLog);
